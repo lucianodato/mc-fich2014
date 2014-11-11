6ee2c9545f4f431b4af67dd114c49a97bbbaa6a5
Luciano <lucianodato@gmail.com>
2014-11-10 21:38:36 -0300

    Ponele que anda para el caso estacionario. Agregado el termino convectivo. Ahora cuenta con convectivo, fuente y difusivo para condiciones dirichlet o neumann.

--- TP4/mvf_ct_1D.m
+++ TP4/mvf_ct_1D.m
@@ -18,49 +18,65 @@ cbn_i = -1;%condicion de borde neumann izquierda
 cbn_d = 1;%condicion de borde neumann derecha
 
 %Problema estacionario
-A = zeros(cant_celdas,cant_celdas);
-b = zeros(cant_celdas,1);
 
 %Matriz del lado derecho (todos los terminos que tienen incognitas fi)
-for i = 1:cant_celdas
-    %Partes de la ecuacion de transmision que colaboran con la matriz
-    switch cant_celdas
-        case 1
-            %Caso que es la primera celda
-            A(i,i) = k/h * -3;
-            A(i,i+1) = k/h;
-        case cant_celdas
-            %Caso ultima celda
-            A(i,i-1) = k/h;
-            A(i,i) = k/h * -3;
-        otherwise
-            %celdas interna
-            A(i,i-1) = k/h;
-            A(i,i) = k/h * -2;
-            A(i,i+1) = k/h;
-    end
-end
-%vector del lado izquierdo (todos los terminos que no tienen icognita fi y condiciones de borde)
+A = zeros(cant_celdas,cant_celdas);
 
 for i = 1:cant_celdas
     %Partes de la ecuacion de transmision que colaboran con la matriz
-    switch cant_celdas
+    switch i
         case 1
             %Caso que es la primera celda
-            if (cbd_i ~= -1)
-                b(i) = -Q*h - 2*k/h * cbd_i;
-            else
-                b(i) = -Q*h - 2*k/h * cbn_i;
+            if (cbd_i ~= -1) %Condicion Dirichlet
+                A(i,i) = v * 1/2 + k/h * -3;
+                A(i,i+1) = v * 1/2 + k/h;
+            else %Condicion Neumann
+                A(i,i) = v + v * 1/2 + k/h * -3;
+                A(i,i+1) = v * 1/2 + k/h;
             end
         case cant_celdas
             %Caso ultima celda
-                        if (cbd_i ~= -1)
+            if (cbd_d ~= -1) %Condicion Dirichlet
+                A(i,i-1) = v * 1/2 + k/h;
+                A(i,i) = v * 1/2 + k/h * -3;
+            else %Condicion Neumann
+                A(i,i-1) = v * 1/2 + k/h;
+                A(i,i) = v + v * 1/2 + k/h * -3;
+            end
+        otherwise
+            %celdas interna
+            A(i,i-1) = v * 1/2 + k/h;
+            A(i,i) = v + k/h * -2;
+            A(i,i+1) = v * 1/2 + k/h;
+    end
+end
+
+
+%vector del lado izquierdo (todos los terminos que no tienen icognita fi y condiciones de borde)
+b = zeros(cant_celdas,1);
+
+for i = 1:cant_celdas
+    %Partes de la ecuacion de transmision que colaboran con la matriz
+    switch i
+        case 1
+            %Caso que es la primera celda
+            if (cbd_i ~= -1) %Condicion Dirichlet
+                b(i) = -Q*h - 2*k/h * cbd_i - v * cbd_i;
+            else %Condicion Neumann
+                b(i) = -Q*h - 2*k/h * cbn_i - v * (-1*cbn_i);%el termino advectivo actua en la direccion de la cara
+            end
+        case cant_celdas
+            %Caso ultima celda
+            if (cbd_d ~= -1)
                 b(i) = -Q*h - 2*k/h * cbd_d;
             else
-                b(i) = -Q*h - 2*k/h * cbn_d;
+                b(i) = -Q*h - 2*k/h * cbn_d - v * (1*cbn_i);%el termino advectivo actua en la direccion de la cara
             end
         otherwise
             %celdas interna
             b(i) = -Q*h;
     end
 end
+
+%Resolucion del sistema
+temp=A\b;
